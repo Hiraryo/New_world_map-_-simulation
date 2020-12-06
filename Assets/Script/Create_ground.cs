73ad99b954b2d917c71df6fe2012cc9cfa9b74b4
New_world_map-_-simulation <Kalium.collecter@gmail.com>
2020-12-06 17:52:11 +0900

    タイルの数を指定する変数名を変更

--- Assets/Script/Create_ground.cs
+++ Assets/Script/Create_ground.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Create_ground : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float row = 5f;
-    public float ren = 10f;
+    public float right = 5f;
+    public float length = 10f;
     private GameObject obj;
     void Start()
     {
@@ -23,10 +23,10 @@ public class Create_ground : MonoBehaviour
 
     void Spawn()//地面生成
     {
-        for (float j = 0; j < 3.6f*row;)//１つ(4x3ブロック = 0.36x0.27)のタイルをX軸方向へrow個並べる、１ブロック：0.09x0.09
+        for (float j = 0; j < 3.6f*right;)//１つ(4x3ブロック = 0.36x0.27)のタイルをX軸方向へrow個並べる、１ブロック：0.09x0.09
         {
             j += 3.6f;//次の列に移動する為に3.6 X軸方向へ移動する
-            for (float i = 0; i < ren; i++)    //Z軸方向へren個並べる
+            for (float i = 0; i < length; i++)    //Z軸方向へren個並べる
             {
                 //プレハブを元にオブジェクトを生成する
                 Instantiate(obj, new Vector3(i*-3.6f+j,0.0f,i*2.7f) , transform.rotation);
